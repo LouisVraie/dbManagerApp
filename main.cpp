ac51198b2b5c60861ae07e49262deebb9cf732f1
LouisVraie <louis.vraie@gmail.com>
2022-02-24 15:46:20 +0100

    Ajout de l'inclusion des fichiers des contributeurs dans le mainwindow.cpp + début du titre de la mainwindow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
 #include "dialogconnexion.h"
 #include <QApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+          db.setHostName("acidalia");
+          db.setDatabaseName("customdb");
+          db.setUserName("mojito");
+          db.setPassword("J0a1m8");
+          bool ok = db.open();
+
     QApplication a(argc, argv);
     DialogConnexion w;
     w.show();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "dialogconnexion.h"
+#include "jonathan.cpp"
+#include "remi.cpp"
+#include "nolann.cpp"
+#include "ugo.cpp"
+#include "theo.cpp"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //setWindowTitle("dbManagerApp - "+databaseName+" - "+databaseIp+" - "+databaseUser);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
