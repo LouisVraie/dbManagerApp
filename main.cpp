c32bc66f644392e31d486be37d907541c874ae15
Selecat <lopez.jonathan04000@gmail.com>
2022-02-24 15:19:02 +0100

    ajout fichier dialog connexion + modif main.cpp (execution dialog en premier) + .pro (sql)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
+#include "dialogconnexion.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    DialogConnexion w;
     w.show();
 
     return a.exec();
