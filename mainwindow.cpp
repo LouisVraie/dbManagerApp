66a08bb8fbd66b3ab943881dc0b31adfe8f30172
LouisVraie <louis.vraie@gmail.com>
2022-02-25 14:30:21 +0100

    Ajout de jonathan.cpp et de commentaires

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "dialogconnexion.h"
-//#include "jonathan.cpp"
+#include "jonathan.cpp"
 //#include "remi.cpp"
 #include "nolann.cpp"
 //#include "ugo.cpp"
 //#include "theo.cpp"
 
+/**
+ * @brief MainWindow::MainWindow
+ * Constructeur de la MainWindow
+ * @param parent
+ */
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -16,6 +21,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
 }
 
+/**
+ * @brief MainWindow::~MainWindow
+ * Destructeur de la MainWindow
+ */
 MainWindow::~MainWindow()
 {
     delete ui;
