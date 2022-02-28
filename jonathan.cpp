c88ecb2cba106d74269e8e844e5f02da09cf61e3
Selecat <lopez.jonathan04000@gmail.com>
2022-02-28 11:55:15 +0100

    modif bouton Quit mainwindow.ui et ajout methode close mainwindow (jonathan.cpp)

--- jonathan.cpp
+++ jonathan.cpp
@@ -1,1 +1,21 @@
 #include "mainwindow.h"
+
+/**
+ * @brief MainWindow::on_action_Quiter_triggered
+ * quitte l'application
+ */
+
+
+//mainwindow.cpp
+void MainWindow::on_action_Quiter_triggered()
+{
+    close();
+}
+
+//mainwindow.h
+
+/**
+ * @brief on_action_Quiter_triggered
+ * quitte l'application
+ */
+void on_action_Quiter_triggered();
