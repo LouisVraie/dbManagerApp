78427e495be71fe0d2cedea6d5b7aff990a876ec
Zerrius <theo.pourroy@free.fr>
2022-02-28 11:43:51 +0100

    Modification theo.cpp

--- theo.cpp
+++ theo.cpp
@@ -11,7 +11,8 @@
  * depuis le plainTextEdit_RequeteSQL.
  * Puis Execute la commande dans la base de donnée.
  */
-void MainWindow::pushButton_Executer(){
+void MainWindow::on_pushButton_Executer_clicked(){
+    qDebug()<<"bouton exec";
     //Déclaration d'une variable de récupération de la requete SQL
     QString requeteSQL;
     QString resultatError;
@@ -21,6 +22,7 @@ void MainWindow::pushButton_Executer(){
     resultatError=query_resultat.lastError().text();
     qDebug()<<"ResultatError"<<resultatError;
     QString sDate = QDateTime::currentDateTime().toString("[dd/MM/yyyy hh:mm:ss]");
+    ui->textBrowser_ResultatRequete->setText(ui->textBrowser_ResultatRequete->toPlainText()+"\r\n"+sDate+" "+resultatError);
     while(query_resultat.next())
     {
             int compteurNbrColonne;
@@ -29,6 +31,7 @@ void MainWindow::pushButton_Executer(){
             QSqlQuery query_resultat2(nomColonne);
             QString resultatDef;
             while(query_resultat2.next())
+            {
                 compteurNbrColonneMax=query_resultat2.value(0).toInt();
                 qDebug()<<compteurNbrColonneMax;
                 for (compteurNbrColonne=0;compteurNbrColonne<compteurNbrColonneMax;compteurNbrColonne++) {
@@ -36,6 +39,6 @@ void MainWindow::pushButton_Executer(){
                     resultatDef=resultatDef+" "+query_resultat.value(compteurNbrColonne).toString();
                 }
             ui->textBrowser_ResultatRequete->setText(ui->textBrowser_ResultatRequete->toPlainText()+"\r\n"+sDate+" "+resultatDef+"\r\n"+"Successful request");
-             }
-    ui->textBrowser_ResultatRequete->setText(ui->textBrowser_ResultatRequete->toPlainText()+"\r\n"+sDate+" "+resultatError);
+            }
+     }
 }
