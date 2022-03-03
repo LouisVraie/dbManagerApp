3f995a68c9caebc76754283d7c778fbb5718bab2
Zerrius <theo.pourroy@free.fr>
2022-03-03 14:42:23 +0100

    Modification de l'affichage des résultats des rèquetes utilisateurs changement du nom du textBrowser.

--- theo.cpp
+++ theo.cpp
@@ -23,10 +23,42 @@ void MainWindow::on_pushButton_Executer_clicked(){
     qDebug()<<"ResultatError"<<resultatError;
     QString sDate = QDateTime::currentDateTime().toString("[dd/MM/yyyy hh:mm:ss]");
     if(resultatError!=" "){
-        ui->textBrowser_ResultatRequete->setText(ui->textBrowser_ResultatRequete->toPlainText()+"\r\n"+sDate+" "+resultatError);
+        ui->textBrowserRequestResult->setText(sDate+" "+resultatError);
     }
     else {
-        ui->textBrowser_ResultatRequete->setText(ui->textBrowser_ResultatRequete->toPlainText()+"\r\n"+sDate+"Successful request");
+        ui->textBrowserRequestResult->setText(sDate+"Successful request");
+    }
+    nomTable = "Achat";//currentTable;/*NOM DA LA TABLE */
+
+    // AFFICHAGE DU HEADER DE LA TABLE
+    nbColonnes = 0;
+    req = "DESC "+nomTable;
+    QSqlQuery query(req);
+    while(query.next())
+    {
+        nbColonnes++;
+        ui->tableWidgetRequestResult->setColumnCount(nbColonnes+1);
+        nomColonne = query.value(0).toString();
+        qDebug()<<nomColonne;
+
+        ui->tableWidgetRequestResult->setHorizontalHeaderItem(nbColonnes,new QTableWidgetItem (nomColonne));
+    }
+    ui->tableWidgetRequestResult->setHorizontalHeaderItem(0,new QTableWidgetItem (""));
+
+    ui->tableWidgetRequestResult->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    // AFFICHAGE DU CONTENU DE LA TABLE
+    nbLignes = 0;
+    while(query_resultat.next())
+    {
+        nbLignes++;
+        ui->tableWidgetRequestResult->setRowCount(nbLignes);
+        ui->tableWidgetRequestResult->setCellWidget(nbLignes-1,0,new QCheckBox);
+        for(int i=1;i<=nbColonnes;i++)
+        {
+            resultat = query_resultat.value(i-1).toString();
+            qDebug()<<resultat;
+            ui->tableWidgetRequestResult->setItem(nbLignes-1,i,new QTableWidgetItem(resultat));
+        }
     }
 }
-
