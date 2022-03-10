133d7596a2f9722c875840212c8e956194889f55
LouisVraie <louis.vraie@gmail.com>
2022-03-10 15:30:40 +0100

    ajout du stretch de tableWidgetRequestResult

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidgetRequestResult->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
 
 /**
