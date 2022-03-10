28dfad524851b492efa8d63032f5e5c557ee904d
Zerrius <theo.pourroy@free.fr>
2022-03-10 15:50:25 +0100

    Ajout des fichier dialogInsertionTheo

---
+++ dialoginsertiontabletheo.h
@@ -0,0 +1,52 @@
+#ifndef DIALOGINSERTIONTABLETHEO_H
+#define DIALOGINSERTIONTABLETHEO_H
+
+#include <QDialog>
+
+namespace Ui {
+class DialogInsertionTableTheo;
+}
+
+class DialogInsertionTableTheo : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogInsertionTableTheo(QWidget *parent = nullptr);
+    ~DialogInsertionTableTheo();
+    QString nomTableSelectionner;
+
+    //creation des lignes
+    void createNewLigne();
+
+    //creation des colonnes
+    void createColonne(QString nomColonne);
+    void getNomTableSelectionner(QString nomTableChoisi);
+
+
+    //affichage console
+    void affichageConsole(QString commande);
+
+    //confirmation exit
+    bool quitConfirm();
+
+
+
+
+private slots:
+    void on_pushButtonAddLigne_clicked();
+
+    void on_pushButtonEnregistrer_clicked();
+
+    void on_pushButtonAnnuler_clicked();
+
+    //fonction qui recupere le nom de la table
+    QString takeNameTable();
+
+    void on_lineEditDatabaseName_textEdited(const QString &arg1);
+
+private:
+    Ui::DialogInsertionTableTheo *ui;
+};
+
+#endif // DIALOGINSERTIONTABLETHEO_H
